27008ef31ca2e76d24ab9380a77d33a8d76feb7a
yojia <3370507176@qq.com>
2022-09-19 09:33:53 +0800

    'first-commit'

---
+++ src/main/java/com/system/community/service/UserService.java
@@ -0,0 +1,19 @@
+package com.system.community.service;
+
+
+import com.system.community.mapper.UserMapper;
+import com.system.community.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User findUserById(int id) {
+        return userMapper.selectById(id);
+    }
+
+}
